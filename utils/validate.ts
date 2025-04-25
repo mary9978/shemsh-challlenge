1dc3da220e6473ac4c39d17688de22d46db8922e
mary9978 <mari.re9978@gmail.com>
2025-04-25 14:01:37 +0330

    feat(helper function) : function to convert rial to toman and Sot to geram

---
+++ utils/validate.ts
@@ -0,0 +1,18 @@
+export function priceFormat(price:string|null){
+     return Number(price).toLocaleString("en-US");
+}
+export function convertRialToToman(rial: string): string {
+  const toman = Math.floor(Number(rial) / 10); 
+  const thousand = Math.floor(toman / 1000);
+  const remainder = toman % 1000;
+  let result = "";
+  if (thousand > 0 ) result += `معادل ${thousand} هزار`;
+  if (remainder === 0 && thousand > 0) result += "تومن";
+  if (remainder > 0 && remainder !== thousand) result += `${thousand > 0 ? " و " : ""}${remainder} تومن`;
+
+  return result || "۰ تومن";
+}
+export function convertSotToGram(sot: string): number {
+  const grams = Number(sot) / 100;
+  return Math.floor(grams * 100) / 100; 
+}
