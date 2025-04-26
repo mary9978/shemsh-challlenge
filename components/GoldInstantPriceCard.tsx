a5e1f1e2a635fb6581b6c75b62a95f9093bf7fa1
mary9978 <mari.re9978@gmail.com>
2025-04-26 21:33:44 +0330

    refactor(GoldInstantPriceCard)

--- components/GoldInstantPriceCard.tsx
+++ components/GoldInstantPriceCard.tsx
@@ -6,22 +6,22 @@ import { IoMdArrowDropup } from "react-icons/io";
 import GoldIcon from "@/assets/images/gold-icon.svg";
 function GoldInstantPriceCard({price}: {price:string}) {
   return (
-    <div className="flex mt-4 w-[320px] lg:w-[550px] mx-auto items-center justify-between rounded-md bg-cardBackground p-4">
+    <div className="card-container custom-card">
       <div className="flex gap-2">
         <Image width={32} height={32} src={GoldIcon} alt="shemsh gold" />
         <div className="flex flex-col">
-          <h6 className="text-primary text-[11px] font-medium font-yekanBakh">قیمت حال حاضر یک سوت طلا</h6>
-          <p className="text-secondary text-[10px] font-medium font-yekanBakh">طلای 18 عیار</p>
+          <h6 className="text-primary text-[11px] font-medium font-IRANSansX">قیمت حال حاضر یک سوت طلا</h6>
+          <p className="text-secondary text-[10px] font-medium font-IRANSansX">طلای 18 عیار</p>
         </div>
       </div>
       <div>
         <div className="flex flex-col">
-          <h6 className="text-primary text-[11px] font-medium font-yekanBakh">
+          <h6 className="text-primary text-[11px] font-medium font-IRANSansX">
             <span className="mx-1">{priceFormat(price)}</span>
             ریال
           </h6>
           <div className="flex items-center">
-            <p className="text-success text-[10px] font-medium font-yekanBakh">{priceFormat("0.04")} %</p>
+            <p className="text-success text-[10px] font-medium font-IRANSansX">{priceFormat("0.04")} %</p>
             <IoMdArrowDropup className="w-4 h-4 text-success" />
           </div>
         </div>
