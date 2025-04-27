f75ef54158d0ffe6539096545f06c6e05f32f59e
mary9978 <mari.re9978@gmail.com>
2025-04-27 22:45:50 +0330

    refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import GoldInstantPriceCard from "@/components/GoldInstantPriceCard";
 import GoldTabSwitcher from "@/components/GoldTabSwitcher";
 import GoldPriceForm from "@/components/GoldPriceForm";
 import HeaderTitle from "@/components/HeaderTitle";
-
 export default function Home() {
   const [price, setPrice] = useState<string>("");
-  const [activeTab, setActiveTab] = useState<1 | 2>(1);
-  const [disabledBtn,setDisabledBtn] = useState<boolean>(true);
-  const toggleTab = (tabId: 1 | 2) => setActiveTab(tabId);
-  function handleDisableBtn(priceprop:any ,sot:any){
-    if (Number(priceprop) !== 0 || Number(sot) !==0) setDisabledBtn(false);
-    else{
+  const [activeTab, setActiveTab] = useState<number>(1);
+  const [disabledBtn, setDisabledBtn] = useState<boolean>(true);
+  // Toggle active tab
+  const toggleTab = useCallback((tabId: number) => setActiveTab(tabId), []);
+  // Handle button disable logic
+  const handleDisableBtn = useCallback((priceprop: string, sot: string) => {
+    if (Number(priceprop) !== 0 && Number(sot) !== 0) {
+      setDisabledBtn(false);
+    } else {
       setDisabledBtn(true);
     }
-  }
+  }, []);
+  // Fetch gold price
   useEffect(() => {
     const fetchPrice = async () => {
       try {
@@ -32,7 +35,7 @@ export default function Home() {
     fetchPrice();
     const interval = setInterval(fetchPrice, 30000);
     return () => clearInterval(interval);
-  }, []);
+  }, [price]);
   return (
     <div className="container-fluid flex justify-center h-screen">
       <div className="flex flex-col w-[360px] md:w-[600px] pt-6 border justify-between shadow-xl">
@@ -40,13 +43,9 @@ export default function Home() {
           <HeaderTitle />
           <GoldInstantPriceCard price={price} />
           <GoldTabSwitcher activeTab={activeTab} toggleTab={toggleTab} />
-          {activeTab === 1 && (
-            <div>
-              <GoldPriceForm priceprop={price} disabledBtnFn={handleDisableBtn} />
-            </div>
-          )}
+          {activeTab === 1 && <GoldPriceForm priceprop={price} disabledBtnFn={handleDisableBtn} />}
         </div>
-        <button className={`button--style ${disabledBtn ? "disabled":""} mb-4 mx-2 py-2`}>
+        <button className={`button--style ${disabledBtn ? "disabled" : ""} mb-4 mx-2 py-2`}>
           {activeTab === 1 ? "خرید طلا" : "فروش طلا"}
         </button>
       </div>
