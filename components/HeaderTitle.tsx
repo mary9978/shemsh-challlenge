9abd8572543d5f861f2388a183885f9a60737647
mary9978 <mari.re9978@gmail.com>
2025-04-27 22:43:50 +0330

    refactor : HeaderTitle.tsx

--- components/HeaderTitle.tsx
+++ components/HeaderTitle.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { IoIosArrowRoundForward } from 'react-icons/io';
+import React from "react";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
-function HeaderTitle() {
+const HeaderTitle: React.FC = () => {
   return (
-    <div className="flex gap-8 jus items-center">
+    <div className="flex items-center">
       <IoIosArrowRoundForward className="icon--style ms-4" />
       <h6 className="title">خرید و فروش طلا</h6>
     </div>
   );
-}
+};
 
-export default HeaderTitle
+export default HeaderTitle;
