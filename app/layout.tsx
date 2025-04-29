a28132b46b01427db280b28bc24fb4007c924041
mary9978 <mari.re9978@gmail.com>
2025-04-29 15:17:02 +0330

    feat(add constant) : add min and max sot and price constant

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { APP_TITLE } from "@/utils/constants";
 
 export const metadata: Metadata = {
-  title: "شمش | اپلیکیشن خرید طلا",
+  title: APP_TITLE,
   description: "به راحتی خرید و فروش طلای خود را به ما بسپارید",
 };
 
---
+++ utils/constants.ts
@@ -0,0 +1,4 @@
+export const APP_TITLE = "شمش | اپلیکیشن خرید و فروش طلا";
+export const MAX_SOT = 60000;
+export const MIN_SOT = 1;
+export const MAX_TOMAN = 200000000;
